0dae185fd35ea55b45d6118a7961501798c01df0
Joe <joeschwartz0@gmail.com>
2025-07-07 22:01:47 -0700

    Fix download path to /tmp for Railway

--- server/app.js
+++ server/app.js
@@ -282,7 +282,7 @@ ${invoiceText}
 });
 
 app.get('/api/download/:filename', (req, res) => {
-  const file = path.join('/tmp', req.params.filename);
+  const file = path.join('/tmp', req.params.filename);  // read from /tmp!
   res.download(file);
 });
 
