8523e27e431faaafc3478e84073a9d02c9c3b7e2
Joe <joeschwartz0@gmail.com>
2025-07-12 12:07:05 -0700

    Revert to default Clerk deployment config

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ClerkProvider } from '@clerk/clerk-react';
 
-const clerkPubKey = "pk_live_Y2xlcmsuc2VydmljZWNpcGhlci5jb20k";
+const clerkPubKey = "pk_test_cG9zaXRpdmUtc2F0eXItNzcuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <ClerkProvider
-    publishableKey={clerkPubKey}
-    frontendApi="clerk.servicecipher.com"
-  >
+  <ClerkProvider publishableKey={clerkPubKey}>
     <App />
   </ClerkProvider>
 );
