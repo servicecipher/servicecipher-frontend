1696d022ab4d366ce9c45ac7cae4105d45c37afb
Joe <joeschwartz0@gmail.com>
2025-07-23 21:06:19 -0700

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,12 @@
 import React from "react";
 import UploadForm from "./UploadForm";
 import "./App.css";
-import allowedEmails from "./allowed_emails.json";
 import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton, useUser } from "@clerk/clerk-react";
 
 // Put your Clerk publishable key here or in index.js as you've already done
 // const clerkPubKey = "pk_test_..."; // not needed if in index.js
 
 function ApprovedUploadOnly({ children }) {
-  const { user } = useUser();
-  const email = user?.primaryEmailAddress?.emailAddress || "";
-
-  const isApproved = allowedEmails
-    .map(e => e.toLowerCase())
-    .includes(email.toLowerCase());
-
-  if (!isApproved) {
-    return (
-      <p style={{ color: "red", marginTop: 32 }}>
-        Sorry, you are not approved to use the upload feature.
-        <br />
-        Please email{" "}
-        <a
-          href="mailto:support@servicecipher.com"
-          style={{ color: "#6c5ce7", textDecoration: "underline" }}
-        >
-          support@servicecipher.com
-        </a>{" "}
-        to sign up!
-      </p>
-    );
-  }
-  // Render child components if approved
   return children;
 }
 
